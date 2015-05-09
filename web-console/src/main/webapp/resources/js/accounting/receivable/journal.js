42565df1c30b05e14b90f2cfe350eef105de7716
redbrick9 <redbrick9@gmail.com>
2015-05-09 15:22:43 +0800

    Adding i18n.

--- web-console/src/main/webapp/resources/js/accounting/receivable/journal.js
+++ web-console/src/main/webapp/resources/js/accounting/receivable/journal.js
@@ -5,19 +5,36 @@ var JOURNALS = [
     {order: 'SA100051', qtyOfBox: '4', paidAmt: '1090.29'}
 ]
 
+
+var i18n = {
+  locales: ["zh"],
+  messages: {
+    crj_order: "CRJ Order"
+  }
+};
+
+var Container = React.createFactory(global.ContainerComponent);
+React.renderToString(
+  Container({
+    locales: i18n.locales,
+    messages: i18n.messages
+  })
+);
+
+var IntlMixin = ReactIntl.IntlMixin;
+var FormattedMessage = ReactIntl.FormattedMessage;
+
 var JournalRow = React.createClass({
     render: function() {
         return ( 
             <tr>
-                <td>{this.props.journal.order.orderNo}</td>
-                <td>{this.props.journal.qtyOfBox}</td>
-                <td>{this.props.journal.paidAmt}</td>
-            </tr>
-        );
+                <td>{this.props.journal.order.orderNo}</td> <td>{this.props.journal.qtyOfBox}</td> <td>{this.props.journal.paidAmt}</td> </tr>);
     }
 })
 
 var JournalsTableBody = React.createClass({
+    mixins: [IntlMixin],
+
     getInitialState: function() {
         return {data: []};
     },
@@ -41,7 +58,7 @@ var JournalsTableBody = React.createClass({
             <table className="table table-striped table-hover table-condensed table-bordered table-16">
                 <thead>
                     <tr>
-                        <th>Order</th>
+                        <th><FormattedMessage message={this.getIntlMessage('crj_order')} /></th>
                         <th>Qty of Box</th>
                         <th>Paid Amt</th>
                     </tr>
