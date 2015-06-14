3b84f489a57f690176362810682c751810d7d634
redbrick9 <redbrick9@gmail.com>
2015-06-14 22:11:09 +0800

    Renaming Order RESTful controller name.

--- order/order/src/main/java/liquid/order/restfulapi/OrderRestController.java
+++ order/order/src/main/java/liquid/order/restfulapi/OrderRestController.java
@@ -1,38 +1,46 @@
 package liquid.order.restfulapi;
 
+import liquid.core.model.SearchBarForm;
 import liquid.operation.domain.Customer;
 import liquid.operation.service.CustomerService;
-import liquid.order.domain.ReceivingOrder;
-import liquid.order.service.ReceivingOrderServiceImpl;
-import liquid.core.model.SearchBarForm;
+import liquid.order.domain.Order;
+import liquid.order.domain.ServiceItem;
+import liquid.order.service.OrderService;
+import liquid.process.service.TaskService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 /**
- * Created by Tao Ma on 2/25/15.
+ * Created by Tao Ma on 12/9/14.
  */
 @Controller
-@RequestMapping("/api/value_added_order")
-public class ApiValueAddedOrderController {
+@RequestMapping("/api/order")
+public class OrderRestController {
+    private static final Logger logger = LoggerFactory.getLogger(OrderRestController.class);
+
     @Autowired
     private CustomerService customerService;
 
     @Autowired
-    private ReceivingOrderServiceImpl receivingOrderService;
+    private OrderService orderService;
+
+    @Autowired
+    private TaskService taskService;
 
     @RequestMapping(method = RequestMethod.GET, params = "text")
     @ResponseBody
     public Iterable<SearchBarForm> listByName(@RequestParam String text) {
+        logger.debug("text: {}", text);
         Iterable<Customer> customers = customerService.findByQueryNameLike(text);
         List<SearchBarForm> searchBarForms = new ArrayList<>();
         for (Customer customer : customers) {
@@ -44,8 +52,8 @@ public class ApiValueAddedOrderController {
         }
 
         PageRequest pageRequest = new PageRequest(0, 10, new Sort(Sort.Direction.DESC, "id"));
-        Page<ReceivingOrder> page = receivingOrderService.findByOrderNoLike(text, pageRequest);
-        for (ReceivingOrder order : page.getContent()) {
+        Page<Order> page = orderService.findByOrderNoLike(text, pageRequest);
+        for (Order order : page.getContent()) {
             SearchBarForm searchBarForm = new SearchBarForm();
             searchBarForm.setId(order.getId());
             searchBarForm.setType("order");
@@ -54,5 +62,22 @@ public class ApiValueAddedOrderController {
         }
 
         return searchBarForms;
+    }
+
+    @RequestMapping(value = "/{id}/service-item", method = RequestMethod.GET)
+    @ResponseBody
+    public Set<ServiceItem> listServiceItems(@PathVariable Long id) {
+        Order order = orderService.find(id);
+        return order.getServiceItems();
+    }
+
+    @RequestMapping(value = "/{id}/service-item", method = RequestMethod.POST, consumes = "application/json")
+    @ResponseBody
+    public void saveServiceItem(@PathVariable Long id, @RequestBody ServiceItem serviceItem) {
+        logger.debug("ServiceItem: {}", serviceItem);
+        Order order = orderService.find(id);
+        order.getServiceItems().remove(serviceItem);
+        order.getServiceItems().add(serviceItem);
+        orderService.save(order);
     }
 }
--- order/order/src/main/java/liquid/order/restfulapi/ValueAddedOrderRestController.java
+++ order/order/src/main/java/liquid/order/restfulapi/ValueAddedOrderRestController.java
@@ -1,46 +1,38 @@
 package liquid.order.restfulapi;
 
-import liquid.core.model.SearchBarForm;
 import liquid.operation.domain.Customer;
 import liquid.operation.service.CustomerService;
-import liquid.order.domain.Order;
-import liquid.order.domain.ServiceItem;
-import liquid.order.service.OrderService;
-import liquid.process.service.TaskService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import liquid.order.domain.ReceivingOrder;
+import liquid.order.service.ReceivingOrderServiceImpl;
+import liquid.core.model.SearchBarForm;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Set;
 
 /**
- * Created by Tao Ma on 12/9/14.
+ * Created by Tao Ma on 2/25/15.
  */
 @Controller
-@RequestMapping("/api/order")
-public class ApiOrderController {
-    private static final Logger logger = LoggerFactory.getLogger(ApiOrderController.class);
-
+@RequestMapping("/api/value_added_order")
+public class ValueAddedOrderRestController {
     @Autowired
     private CustomerService customerService;
 
     @Autowired
-    private OrderService orderService;
-
-    @Autowired
-    private TaskService taskService;
+    private ReceivingOrderServiceImpl receivingOrderService;
 
     @RequestMapping(method = RequestMethod.GET, params = "text")
     @ResponseBody
     public Iterable<SearchBarForm> listByName(@RequestParam String text) {
-        logger.debug("text: {}", text);
         Iterable<Customer> customers = customerService.findByQueryNameLike(text);
         List<SearchBarForm> searchBarForms = new ArrayList<>();
         for (Customer customer : customers) {
@@ -52,8 +44,8 @@ public class ApiOrderController {
         }
 
         PageRequest pageRequest = new PageRequest(0, 10, new Sort(Sort.Direction.DESC, "id"));
-        Page<Order> page = orderService.findByOrderNoLike(text, pageRequest);
-        for (Order order : page.getContent()) {
+        Page<ReceivingOrder> page = receivingOrderService.findByOrderNoLike(text, pageRequest);
+        for (ReceivingOrder order : page.getContent()) {
             SearchBarForm searchBarForm = new SearchBarForm();
             searchBarForm.setId(order.getId());
             searchBarForm.setType("order");
@@ -62,22 +54,5 @@ public class ApiOrderController {
         }
 
         return searchBarForms;
-    }
-
-    @RequestMapping(value = "/{id}/service-item", method = RequestMethod.GET)
-    @ResponseBody
-    public Set<ServiceItem> listServiceItems(@PathVariable Long id) {
-        Order order = orderService.find(id);
-        return order.getServiceItems();
-    }
-
-    @RequestMapping(value = "/{id}/service-item", method = RequestMethod.POST, consumes = "application/json")
-    @ResponseBody
-    public void saveServiceItem(@PathVariable Long id, @RequestBody ServiceItem serviceItem) {
-        logger.debug("ServiceItem: {}", serviceItem);
-        Order order = orderService.find(id);
-        order.getServiceItems().remove(serviceItem);
-        order.getServiceItems().add(serviceItem);
-        orderService.save(order);
     }
 }
