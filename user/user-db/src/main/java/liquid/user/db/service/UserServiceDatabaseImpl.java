50df4f3a7b308b78527a5f46644c3d581be2f166
redbrick9 <redbrick9@gmail.com>
2016-01-28 08:52:10 +0800

    Hot fix for user registration.

--- user/user-db/src/main/java/liquid/user/db/service/UserServiceDatabaseImpl.java
+++ user/user-db/src/main/java/liquid/user/db/service/UserServiceDatabaseImpl.java
@@ -107,6 +107,8 @@ public class UserServiceDatabaseImpl implements UserService {
     @Override
     public User find(String uid) {
         UserProfile profile = userProfileRepository.findOne(uid);
+        if(null == profile)
+            return null;
         User user = convertTo(profile);
         return user;
     }
