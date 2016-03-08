14ff7cbad36cc1922f5d92b734aeec014fc4526b
redbrick9 <redbrick9@gmail.com>
2016-03-08 09:28:37 +0800

    Fixing #298. Abstracting order data model and removing pick-up order.

--- order/order-api/src/main/java/liquid/order/domain/Order.java
+++ order/order-api/src/main/java/liquid/order/domain/Order.java
@@ -51,6 +51,11 @@ public class Order extends BaseOrder {
     @JoinColumn(name = "ORDER_ID", referencedColumnName = "ID")
     private List<ServiceItem> serviceItems = new ArrayList<>();
 
+    @Valid
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinColumn(name = "ORDER_ID", referencedColumnName = "ID")
+    private List<ReceivingContainer> containers;
+
     public int getTradeType() {
         return tradeType;
     }
@@ -121,6 +126,14 @@ public class Order extends BaseOrder {
 
     public void setServiceItems(List<ServiceItem> serviceItems) {
         this.serviceItems = serviceItems;
+    }
+
+    public List<ReceivingContainer> getContainers() {
+        return containers;
+    }
+
+    public void setContainers(List<ReceivingContainer> containers) {
+        this.containers = containers;
     }
 
     @Override
