ff328a1ded669788ec799b52088ca31d780904aa
redbrick9 <redbrick9@gmail.com>
2016-04-10 21:17:51 +0800

    Improving sending truck task #312. For removal of shipping plan.

--- process/process-web/src/main/java/liquid/process/handler/AdjustPriceHandler.java
+++ process/process-web/src/main/java/liquid/process/handler/AdjustPriceHandler.java
@@ -1,9 +1,24 @@
 package liquid.process.handler;
 
+import liquid.accounting.domain.ReceivableSummary;
+import liquid.accounting.service.ReceivableSummaryService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+
+import java.util.Map;
 
 /**
  * Created by Tao Ma on 6/8/15.
  */
 @Component
-public class AdjustPriceHandler extends AbstractOrderPriceHandler {}
+public class AdjustPriceHandler extends AbstractOrderPriceHandler {
+    @Autowired
+    private ReceivableSummaryService receivableSummaryService;
+
+    @Override
+    public void preComplete(String taskId, Map<String, Object> variableMap) {
+        Long orderId = taskService.getOrderIdByTaskId(taskId);
+        ReceivableSummary receivableSummaryEntity = receivableSummaryService.findByOrderId(orderId);
+        variableMap.put("salesPrice", "CNY: " + receivableSummaryEntity.getCny() + "; USD: " + receivableSummaryEntity.getUsd());
+    }
+}
